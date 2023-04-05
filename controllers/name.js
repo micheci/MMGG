5cdeab94850b5bf58d33d5cab2d983d2e89c5769
micheci <michecimartinez@gmail.com>
2023-04-05 09:48:02 -0500

    adding routing to backend

--- controllers/name.js
+++ controllers/name.js
@@ -20,7 +20,15 @@ module.exports = {
         let profilePicURL="http://ddragon.leagueoflegends.com/cdn/13.6.1/img/profileicon/"+picID+'.png';
         console.log(profilePicURL)
 
-        
+        //regionSelector
+        let regions=['br1.api.riotgames.com','eun1.api.riotgames.com'
+        ,'euw1.api.riotgames.com','jp1.api.riotgames.com','kr.api.riotgames.com'
+        ,'la1.api.riotgames.com','la2.api.riotgames.com','na1.api.riotgames.com','oc1.api.riotgames.com'
+        ,'tr1.api.riotgames.com','ru.api.riotgames.com','ph2.api.riotgames.com',
+        'sg2.api.riotgames.com','th2.api.riotgames.com','tw2.api.riotgames.com']
+        valueRegion=req.body.selectpicker
+        console.log(valueRegion)
+
         //Get winrates
         //https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/_h28nu-QHYA4fc1Rgazl8PQWBpR93eHFj1eSIbbOFlri5KSd?api_key=RGAPI-93e1c4f6-8a5c-46ee-9125-7d7e2b0a154f
         let id=SummonerNameurlFull.id
@@ -28,6 +36,8 @@ module.exports = {
         const rankedInfo=await fetch(winLost_url);
         const rankedInfoFull=await rankedInfo.json();
         console.log(rankedInfoFull)
+
+
 
         
 
